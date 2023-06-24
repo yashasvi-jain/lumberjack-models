09af3b48bde9405d05629c76b69e0347cb0c3149
yashasvi-jain <47085019+yashasvi-jain@users.noreply.github.com>
2023-06-24 10:48:16 -0400

    Added the core models and enums.

---
+++ models/enums/languages.py
@@ -0,0 +1,92 @@
+from enum import Enum
+
+
+class Language(Enum):
+    """Enumerates programming languages."""
+
+    assembly = "assembly"
+    """Assembly language."""
+
+    c = "c"
+    """C programming language."""
+
+    c_plus_plus = "c++"
+    """C++ programming language."""
+
+    c_sharp = "c#"
+    """C# programming language."""
+
+    cobol = "cobol"
+    """COBOL programming language."""
+
+    d = "d"
+    """D programming language."""
+
+    dart = "dart"
+    """Dart programming language."""
+
+    erlang = "erlang"
+    """Erlang programming language."""
+
+    fortran = "fortran"
+    """Fortran programming language."""
+
+    go = "go"
+    """Go programming language."""
+
+    haskell = "haskell"
+    """Haskell programming language."""
+
+    html = "html"
+    """HTML programming language."""
+
+    java = "java"
+    """Java programming language."""
+
+    javascript = "javascript"
+    """JavaScript programming language."""
+
+    julia = "julia"
+    """Julia programming language."""
+
+    kotlin = "kotlin"
+    """Kotlin programming language."""
+
+    lua = "lua"
+    """Lua programming language."""
+
+    matlab = "matlab"
+    """MATLAB programming language."""
+
+    objective_c = "objective-c"
+    """Objective-C programming language."""
+
+    pascal = "pascal"
+    """Pascal programming language."""
+
+    perl = "perl"
+    """Perl programming language."""
+
+    php = "php"
+    """PHP programming language."""
+
+    python = "python"
+    """Python programming language."""
+
+    r = "r"
+    """R programming language."""
+
+    ruby = "ruby"
+    """Ruby programming language."""
+
+    rust = "rust"
+    """Rust programming language."""
+
+    scala = "scala"
+    """Scala programming language."""
+
+    swift = "swift"
+    """Swift programming language."""
+
+    typescript = "typescript"
+    """TypeScript programming language."""
