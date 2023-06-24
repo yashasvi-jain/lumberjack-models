9d02b7039782f4f88268d63adca5f42fa8792db0
yashasvi-jain <47085019+yashasvi-jain@users.noreply.github.com>
2023-06-24 16:29:55 -0400

    Fixed imports

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,4 @@
-from application import Application, AppSuite
-from log import Log
+__version__ = '1.0.1'
+
+from models.application import Application, AppSuite
+from models.log import Log
--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import find_packages, setup
+from models import __version__
 
 setup(
     name='Models',
-    version='1.0.0',
+    version=__version__,
     description='The Lumberjack Models library.',
     keywords=['lumberjack', 'models', 'package'],
     packages=find_packages(),
