501bc64a27811a3663fab0053fb0e89873560a84
yashasvi-jain <47085019+yashasvi-jain@users.noreply.github.com>
2023-06-24 16:47:48 -0400

    Fixed versioning

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,2 @@
-__version__ = '1.0.1'
-
 from models.application import Application, AppSuite
 from models.log import Log
--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import find_packages, setup
-from models import __version__
+
+__version__ = '1.0.1'
 
 setup(
     name='Models',
     version=__version__,
     description='The Lumberjack Models library.',
     keywords=['lumberjack', 'models', 'package'],
+    url='https://github.com/yashasvi-jain/lumberjack-models',
     packages=find_packages(),
     install_requires=[
         'pydantic',
