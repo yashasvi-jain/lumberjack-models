98a9a5c91598ca4407c6c7a322a84f5e1870805e
Yashasvi Jain <47085019+yashasvi-jain@users.noreply.github.com>
2023-06-24 15:57:59 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name='Models',
     version='1.0.0',
     description='The Lumberjack Models library.',
     keywords=['lumberjack', 'models', 'package'],
-    packages=[
-        'models',
-        ],
+    packages=find_packages(),
     install_requires=[
         'pydantic',
         'python-dotenv'
