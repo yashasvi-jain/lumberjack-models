ef640324327cf7324d1a7cf5d63f6ee0f1c3462d
yashasvi-jain <47085019+yashasvi-jain@users.noreply.github.com>
2023-09-27 18:29:47 -0400

    Reset verison

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='jackmodels',
+    version='0.0.1',
     description='The Lumberjack Models library.',
     keywords=['lumberjack', 'models', 'package'],
     url='https://github.com/yashasvi-jain/lumberjack-models',
